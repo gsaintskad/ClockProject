597a911d2d2bb1d982b438365e866213598db5aa
Hlib Bezuhlyi <30008@s.pm.szczecin.pl>
2024-07-24 20:19:38 +0200

    pre arrows clock

--- clock.js
+++ clock.js
@@ -8,33 +8,94 @@ class Clock extends HTMLElement{
         const shadow=this.attachShadow({mode:"open"});
         
         //css file connecting
-        const style = document.createElement('style');
-        style.textContent = `
-        @import url('src/styles/clockStyle.css');
-        `;
-        shadow.appendChild(style);
+        this.connectCSS(shadow,`@import url('src/styles/clockStyle.css');`);
         
         
         //clock container creation
-        const clockContainer=document.createElement("div");
-        clockContainer.id="clockContainer";
-       
-        
-        clockContainer.classList.add("clockContainer");
+        this.clockContainer=document.createElement("div");
+        this.clockContainer.id="clockContainer";
+        this.clockContainer.classList.add("clockContainer");
+
+        this.initClock(shadow);
         
 
 
 
-        this.timeHolder=document.createElement("h1");
-        this.timeHolder.id="timeHolder";
-        this.timeHolder.classList.add("timeHolderCSS");
-        clockContainer.appendChild(this.timeHolder);
-        
-        shadow.appendChild(clockContainer);
+        // this.timeHolder=document.createElement("h1");
+        // this.timeHolder.id="timeHolder";
+        // this.timeHolder.classList.add("timeHolderCSS");
+        // clockContainer.appendChild(this.timeHolder);
+        this.addTimeHolder(this.clockContainer);
+
+
+        shadow.appendChild(this.clockContainer);
         
         
         
     }
+    connectCSS(shadow,url){
+        const style = document.createElement('style');
+        style.textContent = url;
+        shadow.appendChild(style);
+    }
+    addTimeHolder(clockContainer=this.clockContainer){
+        
+        this.timeHolder=document.createElement("h1");
+        this.timeHolder.id="timeHolder";
+        this.timeHolder.classList.add("timeHolderCSS");
+        clockContainer.appendChild(this.timeHolder);
+    }
+    initClock(shadow){
+        const clockDiv=document.createElement("div");
+        clockDiv.classList.add("clockDiv");
+        const initHoursMarkers=(shadow)=>{
+            const myDivs=[];
+            const mySubDivs=[];
+  
+  
+            for(let i=0;i<6;i++){
+                myDivs.push(document.createElement("div"));
+                myDivs[i].id=`myDiv${i}`;
+            }
+            for(let i=0;i<12;i++){
+                mySubDivs.push(document.createElement("div"));
+            }
+            
+            mySubDivs.forEach(subDiv => subDiv.classList.add("subHoursMarksDivStyle"));
+            const RotatedDivsStyle = document.createElement('style');
+            
+            myDivs.forEach((div,i) => {
+                
+                 
+                div.classList.add("hoursMarksDivStyle");
+                div.appendChild(mySubDivs[2*i]);
+                div.appendChild(mySubDivs[2*i+1]);
+                
+               // div.style.transform = `rotate(${i*30}deg)`;
+                RotatedDivsStyle.textContent+=`#myDiv${i}{transform: rotate(${i*30}deg);}\n`
+                    
+                clockDiv.appendChild(div);
+                  
+                
+              });
+            shadow.appendChild(RotatedDivsStyle);
+            
+        }
+        const initArrows=shadow=>{
+
+        }
+
+        initHoursMarkers(shadow);
+        initArrows(shadow);
+        this.clockContainer.appendChild(clockDiv);
+    }
+    
+
+
+
+
+
+
     updateClockState=function(){
         const time=new Date();
      
@@ -69,5 +130,17 @@ const clockElement = document.createElement('custom-clock');
 document.body.appendChild(clockElement);
 
 clockElement.runTimer();
+
+// const clockArray=[];
+
+// for (let i = 0; i < 10; i++) {
+    
+//     clockArray.push(document.createElement('custom-clock'));
+    
+// }
+// clockArray.forEach((clock)=>{
+//     document.body.appendChild(clock);
+//     clock.runTimer();
+// });
 
 
